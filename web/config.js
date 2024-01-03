175f7a72bfda67750963fb8d882e2c05a8b83d87
Mathieu Briau <mathieu.briau@gmail.com>
2024-01-03 11:16:32 -0500

    [BugFix] mauvaise configuration

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "5W5 - Programmation Web Avancée",
     description: "Site de référence du cours 5W5",
-    nomUrl: "5W5-web-avancee"
+    nomUrl: "5W5-Web-Avancee"
 }
 
 module.exports = config;
